8790c48fc7e67aa7e1a428665d50ea10009c7388
Dov <dovindustries@gmail.com>
2024-10-06 15:59:27 -0400

    Added some commenting.

--- app/ui/supermint-logo/supermintLogo.tsx
+++ app/ui/supermint-logo/supermintLogo.tsx
@@ -10,6 +10,25 @@ interface SuperMintLogoProps {
   iconSize?: string;
 }
 
+/**
+ * SuperMintLogo Component
+ *
+ * This component renders the SuperMint logo, adapting to light and dark themes.
+ * It uses dynamic imports to load the appropriate logo image based on the current theme.
+ *
+ * @component
+ * @param {Object} props - The component props
+ * @param {boolean} [props.showText=true] - Whether to show the "SuperMint" text
+ * @param {string} [props.textSize="25px"] - The size of the "SuperMint" text
+ * @param {boolean} [props.showIcon=true] - Whether to show the logo icon
+ * @param {string} [props.iconSize="45px"] - The size of the logo icon
+ *
+ * @example
+ * <SuperMintLogo showText={true} textSize="30px" showIcon={true} iconSize="50px" />
+ *
+ * @remarks
+ * The component uses dynamic imports to load the logo images to allow for theme-based logo switching without SSR conflicts.
+ */
 const SuperMintLogo: React.FC<SuperMintLogoProps> = ({
   showText = true,
   textSize = "25px",
