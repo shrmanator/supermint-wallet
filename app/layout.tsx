1cc7d4c0ee7859279e3878aa2285c8c5c7c54386
Dov <dovindustries@gmail.com>
2024-10-07 11:30:10 -0400

    Fixed hydration suppression error.

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
+        suppressHydrationWarning
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
           inter.variable,
