c74a958e33d85b9fef2b5be56520b6194247b10f
Dov <dovindustries@gmail.com>
2024-11-19 11:50:27 -0500

    Added updatenewuserstatus in the welcomemodal.

--- supermint-wallet/components/new-user-welcome-modal.tsx
+++ supermint-wallet/components/new-user-welcome-modal.tsx
@@ -6,20 +6,26 @@ import {
   DialogFooter,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
+import { updateNewUserStatus } from "@/actions/user";
 
 interface WelcomeModalProps {
   isOpen: boolean;
   walletAddress: string;
-  onComplete: (walletAddress: string) => Promise<void>;
+  onClose: () => void;
 }
 
 export function WelcomeModal({
   isOpen,
   walletAddress,
-  onComplete,
+  onClose,
 }: WelcomeModalProps) {
+  const handleComplete = async () => {
+    await updateNewUserStatus(walletAddress, false);
+    onClose();
+  };
+
   return (
-    <Dialog open={isOpen} onOpenChange={() => onComplete(walletAddress)}>
+    <Dialog open={isOpen} onOpenChange={handleComplete}>
       <DialogContent className="sm:max-w-md">
         <DialogHeader>
           <DialogTitle className="text-2xl">
@@ -30,7 +36,7 @@ export function WelcomeModal({
           <p>View and collect digital art while supporting charitable causes</p>
         </div>
         <DialogFooter>
-          <Button onClick={() => onComplete(walletAddress)}>Get Started</Button>
+          <Button onClick={handleComplete}>Get Started</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
