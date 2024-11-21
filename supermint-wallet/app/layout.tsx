1cc4f9ce56e85b7c66cac44a82ae70b61e172723
Dov <dovindustries@gmail.com>
2024-11-21 13:45:33 -0500

    updated nextjs and changed theme to system from light.

--- supermint-wallet/app/layout.tsx
+++ supermint-wallet/app/layout.tsx
@@ -74,7 +74,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
